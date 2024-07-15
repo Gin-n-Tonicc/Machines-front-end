e8491473d0ed8bebbdaadc462e960c8592bd08b1
BelisAliosmanova <benidebelis@gmail.com>
2024-07-15 14:42:01 +0300

    Initial commit

---
+++ src/main/java/com/machines/machines_front_end/MachinesFrontEndApplication.java
@@ -0,0 +1,15 @@
+package com.machines.machines_front_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MachinesFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachinesFrontEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/machines/machines_front_end/MachinesFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machines.machines_front_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachinesFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
